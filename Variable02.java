717c62ea664e64bc0f24b6fb43d6d84c602c9f8b
LswPolyAiSw <162428250+LswPolyAiSw@users.noreply.github.com>
2024-03-11 13:50:08 +0900

    Add files via upload

---
+++ Variable02.java
@@ -0,0 +1,11 @@
+package variable;
+
+public class Variable02 {
+	public static void main(String[] args) {
+		final double PI = 3.14;
+		int r = 5;
+		r = 5;
+		
+		System.out.println(PI * r * r);
+	}
+}
