8ba052c2288619b248f3272d66f2e3d18792df59
LswPolyAiSw <162428250+LswPolyAiSw@users.noreply.github.com>
2024-03-11 14:05:33 +0900

    Add files via upload

---
+++ Print03.java
@@ -0,0 +1,10 @@
+package print;
+
+public class Print03 {
+	public static void main(String[] args) {
+		String myName = "이승우";
+		int myAge = 20;
+		
+		System.out.println("* 이름 : \t" + myName + "\n* 나이 :\t\"" + myAge + "\"");
+	}
+}
