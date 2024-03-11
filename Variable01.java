eaa8b751b977a096171bedafc03f0abb6998f383
LswPolyAiSw <162428250+LswPolyAiSw@users.noreply.github.com>
2024-03-11 13:45:00 +0900

    Add files via upload

---
+++ Variable01.java
@@ -0,0 +1,13 @@
+package variable;
+
+public class Variable01 {
+
+	public static void main(String[] args) {
+		String myName = "이승우";
+		int myAge = 20;
+		
+		System.out.println(myName + "\n" + myAge);
+		
+	}
+
+}
